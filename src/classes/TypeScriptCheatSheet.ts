000e7923ffb677a19ec2271b7b717d856fadfc38
jasonwieczorek <wieczorek.jason@gmail.com>
2022-06-22 21:06:22 -0500

    adding a TypeScript cheat sheet

---
+++ src/classes/TypeScriptCheatSheet.ts
@@ -0,0 +1,53 @@
+// Just a simple typescript file that demonstrates some basic concepts
+
+// Common types
+let aString: string = "jason";
+let aNumber: number = 38;
+let aBoolean: boolean = false;
+let anArray: string[] = ["one", "two", "three"];
+let aTuple: [number, string] = [1, "5"];
+
+// Defining an Type Object
+type Person = {
+    name: string,
+    age?: number; // optional
+}
+
+// Extending a type
+type PersonExtended = Person & {
+    gender: string,
+}
+
+// Defining an Interface
+interface Animal {
+    name: string,
+    age?: number; // optional
+}
+
+// Extending an Interface
+interface WaterAnimal extends Animal {
+    hasGills: boolean,
+}
+
+// Note: Interfaces can extend types, Types can extend Interfaces
+
+// Assigning an object to a variable
+let harry: PersonExtended = {
+    name: "Harry",
+    age: 23,
+    gender: "male"
+}
+
+// An array of people
+let lotsOfPeople: PersonExtended[] = [harry, harry];
+
+// unions of number and string
+let union: string | number = "test";
+union = 25;
+
+// simple function
+function printName(name: string) {
+    console.log(name);
+}
+
+export {}
